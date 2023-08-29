9e059b0aa3b324758bbad0da7611dcd803d39f63
git-user <git-email>
2023-08-29 02:28:11 +0000

    autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../pktest/'))
 
+extensions = [
+    'sphinx.ext.autodoc',
+]
 html_theme = "sphinx_rtd_theme"
 source_suffix = [
     ".md",
